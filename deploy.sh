970708efe1f0ec9c72b44ebbab9e7e682b4ce2a0
ooPG233 <oopg@foxmail.com>
2022-02-08 16:38:43 +0800

    edit

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:oopg233/The-Mythical-Man-Month-zh.git master:gh-pages
+git push -f git@github.com:oopg233/The-Mythical-Man-Month-zh.git master:gh-page
 
 cd -
